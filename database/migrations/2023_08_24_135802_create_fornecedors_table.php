3e001a7d585a6bf2d6d22ffa0d13cf45bc9b3123
Jairo da Silva <silva_jairo2009@yahoo.com.br>
2023-08-25 18:30:28 -0300

    ...

--- database/migrations/2023_08_24_135802_create_fornecedors_table.php
+++ database/migrations/2023_08_24_135802_create_fornecedors_table.php
@@ -11,9 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('fornecedores', function (Blueprint $table) {
+        Schema::create('fornecedors', function (Blueprint $table) {
             $table->id();
             $table->string('name',50);
+            $table->string('site',80);
+            $table->string('uf',50);
+            $table->string('email',80);
             $table->timestamps();
         });
     }
@@ -23,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('fornecedores');
+        Schema::dropIfExists('fornecedors');
     }
 };
